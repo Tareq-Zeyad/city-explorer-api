ad3d67e44b78a2d79d5187ad20ecbb9de89128cd
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-11 17:38:33 +0300

    memory added to server side

--- movies.js
+++ movies.js
@@ -1,28 +1,38 @@
 'use strict';
 const movieAPI = process.env.REACT_APP_MOVIES_KEY;
 const axios = require('axios');
+const { query } = require('express');
+
+let cashMemory = {};
+
 
 function movieData(req, res) {
     let search = req.query.searchQuery;
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPI}&query=${search}`;
 
-    axios
-        .get(url)
-        .then(response => {
-            // console.log(response.data.results);
-    
-            let film = response.data.results.map(item => {
-                // console.log(item);
-                return new Movie(item);
+    // check: do I have the data ! otherwise I will hit the API
+    if (cashMemory[query] !== undefined) {
+        res.send(cashMemory[query]);
+    } else {
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPI}&query=${search}`;
+
+        axios
+            .get(url)
+            .then(response => {
+                // console.log(response.data.results);
+
+                let film = response.data.results.map(item => {
+                    // console.log(item);
+                    return new Movie(item);
+                })
+                cashMemory[query] = film;
+                res.send(film);
+
+
             })
-            res.send(film);
-
-
-        })
-        .catch(error => {
-            res.send('sorry error ' + error);
-        });
-
+            .catch(error => {
+                res.send('sorry error ' + error);
+            });
+    }
 }
 
 class Movie {
@@ -31,7 +41,7 @@ class Movie {
         this.overview = item.overview;
         this.total_votes = item.total_votes;
         this.popularity = item.popularity;
-        this.image_url =`https://image.tmdb.org/t/p/w500${item.poster_path}`;
+        this.image_url = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
         this.released_on = item.release_date;
     }
 
--- weather.js
+++ weather.js
@@ -3,12 +3,19 @@
 const axios = require('axios');
 const weatherAPI = process.env.REACT_APP_WEATHER_KEY;
 
+let cashMemory = {};
+
 function weatherData(req, res) {
     // console.log(1, req.query);
     // console.log(2, weatherAPI);
 
 
     let search = req.query.searchQuery;
+//  check : do I have the data ? otherwise I will hit the API
+if (cashMemory[query] !== undefined){
+    res.send(cashMemory[query]);
+} else {
+
     let url = `http://api.weatherbit.io/v2.0/forecast/daily?city=${search}&key=${weatherAPI}`
 
     axios
@@ -20,12 +27,14 @@ function weatherData(req, res) {
             })
             // console.log(4, climate);
             res.send(climate);
+            cashMemory[query] = climate;
+
 
         })
         .catch( error => {
             res.send('sorry error');
         });
-
+    }
 
 }
 
